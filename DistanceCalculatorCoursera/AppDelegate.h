780145ec074918015bb202b332b5add952b97729
cenkerdemir <cenkerdemir@yahoo.com>
2016-03-31 13:35:19 -0400

    finalized the app with colors, will try to add a for loop in the callback function

---
+++ DistanceCalculatorCoursera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DistanceCalculatorCoursera
+//
+//  Created by Cenker Demir on 3/29/16.
+//  Copyright © 2016 Cenker Demir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DistanceCalculatorCoursera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DistanceCalculatorCoursera
+//
+//  Created by Cenker Demir on 3/29/16.
+//  Copyright © 2016 Cenker Demir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
